9418469e15b54853ee2a75962154511c9a8046f9
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-01-10 15:08:45 -0500

    genre, country, avg_vote queries functional

---
+++ README.md
@@ -0,0 +1,5 @@
+# Moviedex API
+
+This API lets users search through a large collection of movies and display results based on genre, country, or average rating.
+
+Built with Express and JavaScript.
--- server.js
+++ server.js
@@ -1,12 +1,55 @@
-const express = require('express') ;
+require('dotenv').config();
+const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const MOVIES = require('./movies-data-small.json');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+console.log(process.env.API_TOKEN);
 
-app.use((req, res) => {
-    res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+    console.log('validate bearer token middleware');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+             return res.status(401).json({ error: 'Unauthorized request' });
+           }
+
+    // move to the next middleware
+    next();
 })
+
+function handleGetMovies(req, res) {
+    let response = MOVIES;
+    if (req.query.genre) {
+        response = response.filter(movie =>
+          // case insensitive searching
+          movie.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+        );
+      }
+    
+    // filter our movie by country if country query param is present
+    if (req.query.country) {
+      response = response.filter(movie =>
+        movie.country.toLowerCase().includes(req.query.country.toLowerCase())
+      );
+    }
+
+    if (req.query.avg_vote) {
+      response = response.filter(movie =>
+        movie.avg_vote >= Number(req.query.avg_vote)
+      );
+    }
+    
+      res.json(response);
+    }
+
+app.get('/movie', handleGetMovies);
 
 const PORT = 8000;
 
