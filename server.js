203dd0312dea849cf778668c1d848525700e8941
Tyler Orndorff <orndorfftyler801@gmail.com>
2021-01-11 16:24:39 -0500

    heroku preps

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const cors = require('cors');
 const MOVIES = require('./movies-data-small.json');
 
 const app = express();
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
-console.log(process.env.API_TOKEN);
 
 app.use(function validateBearerToken(req, res, next) {
     const authToken = req.get('Authorization');
@@ -51,8 +51,17 @@ function handleGetMovies(req, res) {
 
 app.get('/movie', handleGetMovies);
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-    console.log(`Server listening at http://localhost:${PORT}`)
+// 4 parameters in middleware, express knows to treat this as error handler
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
 })
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
